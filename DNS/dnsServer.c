a2bdc8e6e79769fbe2007fe8bf447b55d4c4a37f
Murugappan-Sekar <muralidaranmuruga@gmail.com>
2017-09-24 18:35:30 +0000

    Added DNS programs

---
+++ DNS/dnsServer.c
@@ -0,0 +1,64 @@
+//DNS Server
+
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<stdio.h>
+char ip[100][1024],dns[100][1024];
+int main(){
+	socklen_t len;
+	struct sockaddr_in servaddr,cliaddr;
+	int n,sockfd,confd;
+	char buffer[1024];
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd<0){
+		perror("Error in socket creation\n");
+		return 0;
+	}
+	bzero(&servaddr,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr=INADDR_ANY;
+	servaddr.sin_port=htons(5000);
+	if(bind(sockfd,(struct sockaddr*)(&servaddr),sizeof(servaddr))<0){
+		perror("binding error\n");
+		return 0;
+	}
+	listen(sockfd,0);
+	len=sizeof(cliaddr);
+	printf("Enter number of entries in the DNS table : ");
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		printf("Enter the ip and corresponding domain name : ");
+		scanf("%s %s",ip[i],dns[i]);
+	}
+	confd=accept(sockfd,(struct sockaddr*)(&cliaddr),&len);
+	if(confd<0){
+		perror("connection error\n");
+		return 0;
+	}
+
+	
+	while(1){
+		read(confd,buffer,sizeof(buffer));
+		
+		if(strcmp(buffer,"bye")==0){
+			printf("Client terminated the connection\n");
+			break;
+		}
+		printf("Client Requested IP for dns : %s\n",buffer);
+		int temp=0;
+		for(int i=1;i<=n;i++){
+			if(strcmp(dns[i],buffer)==0){
+				temp=1;
+				strcpy(buffer,ip[i]);
+			}
+		}
+		if(temp==0)
+			strcpy(buffer,"No corresponding IP found");
+		write(confd,buffer,sizeof(buffer));
+	}
+	close(confd);
+	close(sockfd);
+	return 0;
+}
