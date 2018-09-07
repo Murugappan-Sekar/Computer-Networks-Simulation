1e4c13491457f4e8db8a2ae6dcf08aaf07aa4b45
Murugappan-Sekar <muralidaranmuruga@gmail.com>
2018-09-07 13:07:43 +0530

    Add files via upload

--- RPC/rpcc.c
+++ RPC/rpcc.c
@@ -1,43 +1,42 @@
+// CLIENT
 //RPC client
-
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<stdio.h>
 #include<string.h>
-int main(){
+int main() {
 	int sockfd;
 	struct sockaddr_in servaddr;
 	char buffer[1024];
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd<0){
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
 		perror("Socker error");
 		return 0;
 	}
-	bzero(&servaddr,sizeof(servaddr));
-	servaddr.sin_family=AF_INET;
-	servaddr.sin_addr.s_addr=INADDR_ANY;
-	servaddr.sin_port=htons(5000);
-	connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(5000);
+	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 	printf("ENTER CMD : ");
 	gets(buffer);
-	write(sockfd,buffer,sizeof(buffer));
-	while(1){
-		read(sockfd,buffer,sizeof(buffer));
-		if(strcmp(buffer,"bye")==0){
+	write(sockfd, buffer, sizeof(buffer));
+	while (1) {
+		read(sockfd, buffer, sizeof(buffer));
+		if (strcmp(buffer, "bye") == 0) {
 			printf("Server terminated the connection\n");
 			break;
 		}
-		printf("RETURNED VALUE IS  : %s\n\n",buffer);
+		printf("RETURNED VALUE IS : %s\n\n", buffer);
 		printf("ENTER CMD(bye to exit) : ");
 		gets(buffer);
-		if(strcmp(buffer,"bye")==0){
-			write(sockfd,buffer,sizeof(buffer));
+		if (strcmp(buffer, "bye") == 0) {
+			write(sockfd, buffer, sizeof(buffer));
 			printf("You terminated the connection\n");
 			break;
 		}
-		write(sockfd,buffer,sizeof(buffer));
-	
+		write(sockfd, buffer, sizeof(buffer));
 	}
 	close(sockfd);
 	return 0;
--- RPC/rpcs.c
+++ RPC/rpcs.c
@@ -1,5 +1,5 @@
+// SERVER
 //RPC Server
-
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -7,72 +7,70 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-void decoder(char ip[]){
-	int num=0;
-	int fst=1500,est=1500;
-	for(int i=0;i<strlen(ip);i++){
-		if(ip[i]=='(')
-			fst=i;
-		else if(ip[i]==')'){
-			if(fst<i)
-				est=i;
+void decoder(char ip[]) {
+	int num = 0;
+	int fst = 1500, est = 1500;
+	for (int i = 0; i < strlen(ip); i++) {
+		if (ip[i] == '(')
+			fst = i;
+		else if (ip[i] == ')') {
+			if (fst < i)
+				est = i;
 			else
-				est=-1;
+				est = -1;
 
 		}
 	}
-	if(est==-1){
-		strcpy(ip,"invalid command");
-	}else{
-		for(int i=fst+1;i<est;i++){
-			num=num*10+(ip[i]-'0');
+	if (est == -1) {
+		strcpy(ip, "invalid command");
+	} else {
+		for (int i = fst + 1; i < est; i++) {
+			num = num * 10 + (ip[i] - '0');
 		}
-		ip[fst]='\0';
-		num=pow(num,2);
-		if(strcmp(ip,"square")!=0){
-			strcpy(ip,"invalid command");	
-		}else
+		ip[fst] = '\0';
+		num = pow(num, 2);
+		if (strcmp(ip, "square") != 0) {
+			strcpy(ip, "invalid command");
+		} else
+			snprintf(ip, sizeof(ip), "%d", num);
 
-			snprintf(ip,sizeof(ip),"%d",num);
 	}
 }
-int main(){
+int main() {
 	socklen_t len;
-	struct sockaddr_in servaddr,cliaddr;
-	int n,sockfd,confd;
+	struct sockaddr_in servaddr, cliaddr;
+	int n, sockfd, confd;
 	char buffer[1024];
-	sockfd=socket(AF_INET,SOCK_STREAM,0);
-	if(sockfd<0){
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
 		perror("Error in socket creation\n");
 		return 0;
+
 	}
-	bzero(&servaddr,sizeof(servaddr));
-	servaddr.sin_family=AF_INET;
-	servaddr.sin_addr.s_addr=INADDR_ANY;
-	servaddr.sin_port=htons(5000);
-	if(bind(sockfd,(struct sockaddr*)(&servaddr),sizeof(servaddr))<0){
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(5000);
+	if (bind(sockfd, (struct sockaddr*)(&servaddr), sizeof(servaddr)) < 0) {
 		perror("binding error\n");
 		return 0;
 	}
-	listen(sockfd,0);
-	len=sizeof(cliaddr);
-	confd=accept(sockfd,(struct sockaddr*)(&cliaddr),&len);
-	if(confd<0){
+	listen(sockfd, 0);
+	len = sizeof(cliaddr);
+	confd = accept(sockfd, (struct sockaddr*)(&cliaddr), &len);
+	if (confd < 0) {
 		perror("connection error\n");
 		return 0;
 	}
-
-	
-	while(1){
-		read(confd,buffer,sizeof(buffer));
-		
-		if(strcmp(buffer,"bye")==0){
+	while (1) {
+		read(confd, buffer, sizeof(buffer));
+		if (strcmp(buffer, "bye") == 0) {
 			printf("Client terminated the connection\n");
 			break;
 		}
-		printf("Client Requested : %s\n",buffer);
+		printf("Client Requested : %s\n", buffer);
 		decoder(buffer);
-		write(confd,buffer,sizeof(buffer));
+		write(confd, buffer, sizeof(buffer));
 	}
 	close(confd);
 	close(sockfd);
